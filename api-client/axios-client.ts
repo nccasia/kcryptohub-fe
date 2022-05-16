1451d4b4c742e91dfc59a48b58752004aa552b04
dungnd193 <dungnd.work@gmail.com>
2022-05-16 16:00:44 +0700

    replace api link by env variable

--- api-client/axios-client.ts
+++ api-client/axios-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "https://kryptohub-be.herokuapp.com",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
