d6b39e66e0b3487ed44c2247b0c4b25513db2e78
pv <79627101+thangngh@users.noreply.github.com>
2022-06-16 20:12:33 +0700

    fix space

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,32 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
-import { Provider } from 'react-redux';
-import store from '@/redux/store';
-import { ToastContainer } from 'react-toastify';
-import { LayoutGuard } from '../layouts/LayoutGuard';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
+import { ToastContainer } from "react-toastify";
+import { LayoutGuard } from "../layouts/LayoutGuard";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <SessionProvider session={pageProps.session}>
-            <Provider store={store}>
-                <LayoutGuard>
-                    <Component {...pageProps} />
-                </LayoutGuard>
-                <ToastContainer
-                    position="top-center"
-                    autoClose={3000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                />
-            </Provider>
-        </SessionProvider>
-    );
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Provider store={store}>
+        <LayoutGuard>
+          <Component {...pageProps} />
+        </LayoutGuard>
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </Provider>
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,53 +1,51 @@
 module.exports = {
-    important: true,
-    content: [
-        './src/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {
-        extend: {
-            colors: {
-                primary: '#17313b',
-            },
-            boxShadow: {
-                '3xl': 'inset 0 1px 1px rgb(0 0 0 / 8%), 0 0 8px rgb(102 175 233 / 60%)',
-            },
-            keyframes: {
-                'slide-in-up': {
-                    from: {
-                        transform: 'translateY(50%)',
-                        opacity: 0,
-                    },
-                    to: {
-                        transform: 'translateY(0)',
-                        opacity: 1,
-                    },
-                },
-                'slide-in-left': {
-                    from: {
-                        transform: 'translateX(50%)',
-                        opacity: 0,
-                    },
-                    to: {
-                        transform: 'translateX(0)',
-                        opacity: 1,
-                    },
-                },
-            },
-            animation: {
-                'slide-in-up': 'slide-in-up 0.5s ease-in-out both',
-                'slide-in-left': 'slide-in-left 0.5s ease-in-out both',
-            },
+  important: true,
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#17313b",
+      },
+      boxShadow: {
+        "3xl":
+          "inset 0 1px 1px rgb(0 0 0 / 8%), 0 0 8px rgb(102 175 233 / 60%)",
+      },
+      keyframes: {
+        "slide-in-up": {
+          from: {
+            transform: "translateY(50%)",
+            opacity: 0,
+          },
+          to: {
+            transform: "translateY(0)",
+            opacity: 1,
+          },
         },
-        screens: {
-            xxs: '424px',
-            xs: '540px',
-            sm: '640px',
-            md: '768px',
-            lg: '1024px',
-            xl: '1280px',
-            '2xl': '1536px',
+        "slide-in-left": {
+          from: {
+            transform: "translateX(50%)",
+            opacity: 0,
+          },
+          to: {
+            transform: "translateX(0)",
+            opacity: 1,
+          },
         },
+      },
+      animation: {
+        "slide-in-up": "slide-in-up 0.5s ease-in-out both",
+        "slide-in-left": "slide-in-left 0.5s ease-in-out both",
+      },
     },
-    plugins: [],
+    screens: {
+      xxs: "424px",
+      xs: "540px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  plugins: [],
 };
