6653641250b625d0b3385fdd00b1d0c13551e5f1
Hong Thang <79627101+thangngh@users.noreply.github.com>
2022-06-27 09:17:59 +0700

    Thangngh/member (#53)
    * member UI
    
    * refactor code
    
    * refactor code again
    
    * refactor code issue: .prettierr
    
    * fix space
    
    * add misstake function maintain ui
    
    * fix build
    
    * get API member with param, invite member, remove member
    
    * maintain ui loading table
    
    * resolved conflicts
    
    Co-authored-by: Trịnh Đức Đại <dai.trinhduc@ncc.asia>

--- redux/store.ts
+++ redux/store.ts
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import ProfileReducer from "@/redux/profileSlice";
 import TeamReducer from "@/redux/teamSlice";
 import { SkillReducer } from "./skillSlice";
-import { MemberReducer } from "@/redux/memberSlice";
+import MemberReducer from "@/redux/memberSlice";
 import TeamProfileReducer from "@/redux/teamProfileSlice";
 import AwardsReducer from "@/redux/awardSlice";
 import DashboardReducer from "@/redux/dashboardSlice";
--- src/pages/team/[teamId]/dashboard/members/index.tsx
+++ src/pages/team/[teamId]/dashboard/members/index.tsx
@@ -1,8 +1,14 @@
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import { getMemberList } from "@/redux/memberSlice";
-import { getMemberSelector } from "@/redux/selector";
+import {
+  addMember,
+  getMemberList,
+  joinTeam,
+  removeMember,
+  resetSuccess,
+} from "@/redux/memberSlice";
+import { getUserInfoSelector } from "@/redux/selector";
 import DashboardLayout from "@/src/layouts/dashboard/Dashboard";
-import { IMemberAddRequest } from "@/type/member/member.type";
+import { emails, IMemberAddRequest } from "@/type/member/member.type";
 import {
   Chip,
   Container,
@@ -12,14 +18,18 @@ import {
   ThemeProvider,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
 import { ToastContainer } from "react-toastify";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import Image from "next/image";
 import iconchecked from "@/src/assets/image/icon-check.svg";
 import iconToolip from "@/src/assets/image/icon-tooltip.svg";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 const theme = createTheme({
   components: {
     MuiOutlinedInput: {
@@ -52,109 +62,58 @@ const mailColor = [
   "bg-pink-500",
   "bg-gray-500",
   "bg-teal-500",
-  "bg-cyan-500",
-  "bg-lime-500",
-  "bg-indigo-700",
-  "bg-pink-700",
-  "bg-gray-700",
-  "bg-teal-700",
-  "bg-cyan-700",
-  "bg-lime-700",
-  "bg-red-700",
-  "bg-orange-700",
-  "bg-yellow-700",
-  "bg-green-700",
-  "bg-blue-700",
-  "bg-lime-300",
-  "bg-indigo-300",
-  "bg-pink-300",
-  "bg-gray-300",
-  "bg-teal-300",
-  "bg-cyan-300",
 ];
 const mailRegexp = new RegExp(
-  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{3,}))$/
+  /^[a-zA-Z0-9.]+@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 );
 
-const mockData = [
-  {
-    id: 1,
-    emailAddress: "thang@gmail.com",
-    inviteStatus: "PENDING",
-    role: "MEMBER",
-    createAt: "2020-01-01T00:00:00.000Z",
-    updateAt: "2020-01-01T00:00:00.000Z",
-    teamId: "1",
-    userId: "1",
-  },
-  {
-    id: 2,
-    emailAddress: "hong@gmail.com",
-    inviteStatus: "ACCEPTED",
-    role: "MEMBER",
-    createAt: "2020-01-01T00:00:00.000Z",
-    updateAt: "2020-01-01T00:00:00.000Z",
-    teamId: "1",
-    userId: null,
-  },
-  {
-    id: 3,
-    emailAddress: "hongs@gmail.com",
-    inviteStatus: "Rejected",
-    role: "MEMBER",
-    createAt: "2020-01-01T00:00:00.000Z",
-    updateAt: "2020-01-01T00:00:00.000Z",
-    teamId: "1",
-    userId: null,
-  },
-];
-
 enum InviteStatus {
-  PENDING = "PENDING",
-  ACCEPTED = "ACCEPTED",
-  REJECTED = "REJECTED",
+  PENDING = "pending",
+  ACCEPTED = "accepted",
+  REJECTED = "rejected",
 }
 
 const Members = () => {
-  const getTag =
-    typeof window !== undefined ||
-    JSON.parse(localStorage.getItem("tag") || "");
-
   const [email, setEmail] = useState<string>("");
-  const [tags, setTags] = useState<string[]>([]);
+  const [tags, setTags] = useState<emails[]>([]);
   const [openPermissions, setOpenPermissions] = useState<boolean>(false);
   const [openStatus, setOpenStatus] = useState<boolean>(false);
-
-  const { register, handleSubmit } = useForm<IMemberAddRequest>();
+  const [disableIvt, setDisableIvt] = useState<boolean>(false);
+  const [success, setSuccess] = useState<boolean>(false);
   const dispatch = useAppDispatch();
+  const actionSuccess = useSelector(
+    (state: RootState) => state.MemberReducer.success
+  );
+  const router = useRouter();
+  const teamId = router.query.teamId;
 
-  const memberList = useAppSelector(getMemberSelector);
+  const memberList = useSelector(
+    (state: RootState) => state.MemberReducer.member
+  );
 
-  useEffect(() => {
-    dispatch(getMemberList());
-  }, [dispatch]);
+  const Owner = useAppSelector(getUserInfoSelector);
 
   const handleOpenPermissions = () => setOpenPermissions(!openPermissions);
   const handleOpenStatus = () => setOpenStatus(!openStatus);
 
-  const onEmailSend = async (email: IMemberAddRequest) => {
-    if (typeof window !== undefined) {
-      localStorage.removeItem("tags");
-    }
-  };
-
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (e.target.value.match(mailRegexp)) {
+      setDisableIvt(true);
+    }
     setEmail(e.target.value);
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.code === "Space" && mailRegexp.test(email)) {
+    if (e.code === "Enter" && mailRegexp.test(email)) {
       setTags((tag) => {
-        const newTag = [...tag, email];
+        const newTag = [
+          ...tag,
+          {
+            email: email,
+            role: "member",
+          },
+        ];
 
-        // if (typeof window !== undefined) {
-        //     localStorage.setItem('tags', JSON.stringify(newTag));
-        // }
         return newTag;
       });
       setEmail("");
@@ -162,9 +121,45 @@ const Members = () => {
     }
   };
 
+  useEffect(() => {
+    dispatch(getMemberList(parseInt(teamId as string)));
+  }, [dispatch, teamId]);
+
+  useEffect(() => {
+    if (actionSuccess === true) {
+      dispatch(resetSuccess());
+      dispatch(getMemberList(parseInt(teamId as string)));
+    }
+  }, [actionSuccess, dispatch, teamId]);
+
+  // useEffect(() => {
+  //   dispatch(joinTeam(parseInt(teamId as string)));
+  // }, [dispatch, teamId]);
+
+  const handleSubmit = async () => {
+    const data: IMemberAddRequest = {
+      teamId: parseInt(teamId as string),
+      members: tags,
+    };
+    await dispatch(addMember(data));
+
+    setSuccess(true);
+  };
+
+  const handleClose = () => {
+    setSuccess(false);
+    setTags([]);
+  };
   const handleDeleteTag = (index: number) => {
     setTags((prev) => prev.filter((_, i) => i !== index));
   };
+
+  const deleteMember = (index: number) => {
+    dispatch(
+      removeMember({ teamId: parseInt(teamId as string), memberId: index })
+    );
+  };
+
   return (
     <DashboardLayout>
       <ThemeProvider theme={theme}>
@@ -173,44 +168,62 @@ const Members = () => {
             <div className="w-full block relative ">
               <div className="my-2">
                 <form
-                  action="#"
+                  onSubmit={(e) => e.preventDefault()}
                   className="shadow-lg block lg:inline-flex py-2 my-3 lg:items-end px-3 bg-white"
                 >
                   <FormControl className="p-2">
                     <h1 className="py-2 text-lg font-semibold">
                       Invite Members
                     </h1>
-                    <span className="py-1 text-sm font-normal">
+                    <span
+                      className={
+                        !success
+                          ? "py-1 text-sm font-normal "
+                          : "py-1 text-sm font-normal hidden"
+                      }
+                    >
                       Add team members via email
                       <span className="opacity-50 ">
-                        ( separate emails with a space if correct format):
+                        ( separate emails with a enter if correct format):
                       </span>
                     </span>
-                    <div className="outline-none border-[1px] border-green-400">
+                    <div
+                      className={
+                        !success
+                          ? "outline-none  border-[1px] border-green-400"
+                          : "outline-none"
+                      }
+                    >
                       {tags.map((tag, index) => (
                         <Chip
                           variant="outlined"
-                          className={`${mailColor[index]} mr-2 my-2 text-white`}
+                          className={
+                            !success
+                              ? `${mailColor[index]} mr-2 my-2 text-white `
+                              : `${mailColor[index]} mr-2 my-2 text-white hidden`
+                          }
                           key={index}
-                          label={tag}
+                          label={tag.email}
                           deleteIcon={
                             <HighlightOffIcon className="w-5 h-5 text-white" />
                           }
                           onDelete={() => handleDeleteTag(index)}
                         />
                       ))}
+
                       <TextareaAutosize
-                        className="w-full h-20 outline-none rounded border-none resize-none p-1"
+                        className={
+                          !success
+                            ? "w-full h-20 outline-none rounded border-none resize-none p-1 "
+                            : "w-full h-20 outline-none rounded border-none resize-none p-1 hidden"
+                        }
                         placeholder="name@profilename.com..."
                         value={email}
+                        onChange={handleChange}
                         onKeyDown={handleKeyDown}
-                        {...register("member", {
-                          required: true,
-                          onChange: handleChange,
-                        })}
                       />
                     </div>
-                    <div className="mr-10 hidden">
+                    <div className={!success ? "mr-10 hidden" : "mr-10"}>
                       <div className="flex justify-center">
                         <span className="pr-3">
                           <Image
@@ -222,25 +235,31 @@ const Members = () => {
                         <div className="flex flex-col">
                           <span>Invites have been sent to:</span>
                           {tags.map((tag, index) => (
-                            <span key={index}>{tag}</span>
+                            <span key={index}>{tag.email}</span>
                           ))}
                         </div>
                       </div>
                     </div>
                   </FormControl>
 
-                  <div className="ml-3 pb-2">
+                  <div className={!success ? "ml-3 pb-2 " : "ml-3 pb-2 hidden"}>
                     <button
                       type="submit"
-                      className="py-2 px-4 border-[1px] border-green-400 rounded shadow"
+                      onClick={handleSubmit}
+                      disabled={!disableIvt ? true : false}
+                      className={
+                        !disableIvt
+                          ? "py-2 px-4 border-[1px] border-green-400 rounded shadow cursor-not-allowed"
+                          : "py-2 px-4 border-[1px] border-green-400 rounded shadow "
+                      }
                     >
                       <span>Invite</span>
                     </button>
                   </div>
 
-                  <div className="py-2 hidden">
+                  <div className={!success ? "py-2 hidden" : "py-2"}>
                     <button
-                      // onClick={handleClose}
+                      onClick={handleClose}
                       className="py-2 px-4 border-[1px] border-green-400 rounded shadow"
                     >
                       <span>Send more invites</span>
@@ -250,7 +269,7 @@ const Members = () => {
               </div>
             </div>
             <div className="bg-white w-full h-full block relative overflow-x-auto shadow-md my-2 drop-shadow">
-              <div className="w-[800px] lg:w-full table p-3">
+              <div className="w-[1200px] table p-3">
                 <h1 className="font-bold text-xl py-1 px-4">Member</h1>
                 <div className="flex items-center justify-center">
                   <div className="w-1/5 px-4 py-2 text-base font-normal">
@@ -338,24 +357,34 @@ const Members = () => {
                   </div>
                 </div>
                 <div className="border-[1px] py-3 flex items-center justify-center">
-                  <div className="w-1/5 px-2 py-2 text-sm font-normal">
+                  <div className="w-1/5 px-2 py-2 text-sm font-normal inline-flex items-center">
                     <span className="pr-2">
                       <AccountCircleIcon className="w-5 h-5" />
                     </span>
-                    <span>{"Username" || "-"}</span>
+                    {Object.entries(Owner).length !== 0 ? (
+                      <span className="text-[#17313b]">{Owner.username}</span>
+                    ) : (
+                      <div className="bg-[#1b08086c] w-full h-[10px] rounded-3xl animate-pulse"></div>
+                    )}
                   </div>
                   <div className="w-1/5 px-4 py-2 text-sm font-normal">
-                    <span className="text-[#17313b]">Email </span>
+                    {Object.entries(Owner).length !== 0 ? (
+                      <span className="text-[#17313b]">
+                        {Owner.emailAddress}
+                      </span>
+                    ) : (
+                      <div className="bg-[#1b08086c] w-full h-[10px] rounded-3xl animate-pulse"></div>
+                    )}
                   </div>
                   <div className="w-1/5 px-4 py-2 text-sm font-normal">
-                    <span className="text-[#17313b]">Manager</span>
+                    <span className="text-[#17313b]">Owner</span>
                   </div>
                   <div className="w-1/5 px-4 py-2 text-sm font-normal"></div>
                   <div className="w-1/5 px-4 py-2 text-sm font-normal"></div>
                 </div>
-                {mockData.map((item, index) => (
-                  <>
-                    <div
+                <ul className="w-full">
+                  {memberList?.map((item, index) => (
+                    <li
                       key={index}
                       className="border-[1px] py-3 flex items-center justify-center"
                     >
@@ -363,7 +392,9 @@ const Members = () => {
                         <span className="pr-2">
                           <AccountCircleIcon className="w-5 h-5" />
                         </span>
-                        <span>{item.userId === null ? "-" : "Username"}</span>
+                        <span>
+                          {item.user === null ? "-" : item.user?.username}
+                        </span>
                       </div>
                       <div className="w-1/5 px-4 py-2 text-sm font-normal">
                         <span className="text-[#17313b]">
@@ -383,19 +414,67 @@ const Members = () => {
                             <span className="!text-xs">Accepted</span>
                           </div>
                         ) : (
-                          <div className="bg-[#333131] text-[#fff] rounded-3xl px-2 py-1 w-[110px] text-center">
-                            <span className="!text-xs">Rejected</span>
+                          <div className="bg-[#ff3d2e] text-[#fff] rounded-3xl px-2 py-1 w-[110px] text-center">
+                            <span className="!text-xs">invite Expired</span>
                           </div>
                         )}
                       </div>
                       <div className="w-1/5 px-4 py-2 text-sm font-normal">
-                        <span className="cursor-pointer hover:underline text-blue-500">
-                          Remove
-                        </span>
+                        {item.inviteStatus === InviteStatus.REJECTED ? (
+                          <>
+                            <span className="cursor-pointer hover:underline text-blue-500">
+                              Resend
+                            </span>
+                            {" | "}
+                            <span
+                              onClick={() => deleteMember(item.id)}
+                              className="cursor-pointer hover:underline text-blue-500"
+                            >
+                              Remove
+                            </span>
+                          </>
+                        ) : (
+                          <span
+                            onClick={() => deleteMember(item.id)}
+                            className="cursor-pointer hover:underline text-blue-500"
+                          >
+                            Remove
+                          </span>
+                        )}
                       </div>
-                    </div>
-                  </>
-                ))}
+                    </li>
+                  )) ?? (
+                    <>
+                      <li className="border-[1px] py-3 flex items-center justify-center animate-pulse">
+                        <div className="w-1/5 px-2 py-2 text-sm font-normal inline-flex items-center">
+                          <span className="pr-2 ">
+                            <AccountCircleIcon className="w-5 h-5" />
+                          </span>
+                          <div className="bg-[#1b08086c] w-full h-[10px] rounded-3xl"></div>
+                        </div>
+                        <div className="w-1/5 px-4 py-2 text-sm font-normal flex">
+                          <span className="bg-[#1b08086c] w-full h-[10px] rounded-3xl"></span>
+                        </div>
+                        <div className=" w-1/5 px-4 py-2 text-sm font-normal flex">
+                          <span className="bg-[#1b08086c] w-full h-[10px] rounded-3xl"></span>
+                        </div>
+                        <div className="w-1/5 px-4 py-2 text-sm font-normal flex">
+                          <span className="bg-[#1b08086c] w-full h-[10px] rounded-3xl"></span>
+                        </div>
+                        <div className=" w-1/5 px-4 py-2 text-sm font-normal flex">
+                          <span className="bg-[#1b08086c] w-full h-[10px] rounded-3xl"></span>
+                        </div>
+                      </li>
+                    </>
+                  )}
+                  {memberList?.length === 0 && (
+                    <>
+                      <li className="border-[1px] py-3 flex items-center justify-center w-full">
+                        <span>No Data</span>
+                      </li>
+                    </>
+                  )}
+                </ul>
               </div>
             </div>
           </div>
