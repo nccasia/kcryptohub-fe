0e97ac5c8e9e3acb32cd3a977c3e832ba70dfce8
pv <79627101+thangngh@users.noreply.github.com>
2022-07-25 16:29:29 +0700

    fix join team with register new account

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -56,6 +56,7 @@ const Register = () => {
   });
   const [validateEmail, setValidate] = React.useState(false);
   const [validateUsername, setValidateUsername] = React.useState(false);
+  const [redirectUrl, setRedirectUrl] = React.useState("/login");
   const onNext = () => {
     if (step === 0) {
       setStep((cur) => cur + 1);
@@ -66,8 +67,8 @@ const Register = () => {
     const query = new URLSearchParams(router.asPath);
     const url = query.has("/login?url")
       ? (query.get("/login?url") as string)
-      : "/login";
-    console.log(url);
+      : redirectUrl;
+
     router.push(url);
   };
 
@@ -98,6 +99,13 @@ const Register = () => {
       });
     }
   };
+  useEffect(() => {
+    if (router.isReady) {
+      if (router.query && router.query.url) {
+        setRedirectUrl(router.query.url as string);
+      }
+    }
+  }, [router.isReady]);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -140,12 +148,12 @@ const Register = () => {
         >
           <div>
             {step === 0 && (
-              <div>
-                <Link href="/login">
+              <div className="cursor-pointer">
+                <div onClick={() => router.back()}>
                   <a>
                     <ArrowBackIosNewIcon />
                   </a>
-                </Link>
+                </div>
               </div>
             )}
             <h1 className="mt-1 text-center text-3xl font-bold text-[#944C00]">
