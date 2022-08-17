f4edbae42af76cc9cddfe863484f1e57b1f5a562
tinpham1510 <tinpham15102000@gmail.com>
2022-08-17 15:28:11 +0700

    fix UI 1

--- src/pages/change-password/index.tsx
+++ src/pages/change-password/index.tsx
@@ -30,7 +30,7 @@ const RePassword = () => {
     handleSubmit,
     reset,
     formState: { errors },
-    watch
+    watch,
   } = useForm<any>({
     resolver: yupResolver(schemaValidation),
     mode: "all",
@@ -39,13 +39,16 @@ const RePassword = () => {
   const [submited, setSubmited] = useState(false);
   const onSubmit = async () => {
     setIsSubmitting(true);
-    if(router.query.token) {
-      try{
-        await authApi.changePassword(watch("password"), router.query.token as string);
+    if (router.query.token) {
+      try {
+        await authApi.changePassword(
+          watch("password"),
+          router.query.token as string
+        );
         toast.success("Password changed successfully!");
         setSubmited(true);
         router.push("/login");
-      } catch(err: any){
+      } catch (err: any) {
         setIsSubmitting(false);
         toast.error(err.response.data.message);
       }
@@ -55,12 +58,12 @@ const RePassword = () => {
     }
   };
   return (
-    <div className="h-auto min-h-full bg-primary flex justify-center xs:items-center px-2 xxs:px-0  py-4">
+    <div className="h-auto min-h-full bg-[#f9fafb] font-jost flex justify-center xs:items-center px-2 xxs:px-0  py-4">
       <div className="max-w-[420px] h-fit w-full animate-slide-in-left">
         <div className="text-center ">
-          <h3 className="text-5xl font-bold text-white">Kryptohub</h3>
+          <h3 className="text-5xl font-bold text-[#606060]">Kryptohub</h3>
         </div>
-        <div className="w-full h-fit max-h-3/4 bg-white p-8 mt-8">
+        <div className="w-full h-fit max-h-3/4 bg-white p-8 mt-8 rounded-3xl shadow-3xl">
           <div
             className="cursor-pointer"
             onClick={() => {
@@ -71,20 +74,20 @@ const RePassword = () => {
           </div>
           {!isSubmitting ? (
             <div className="">
-              <h3 className="text-cyan-900 text-md py-4">
+              <h3 className="text-[#848abd] text-md py-4">
                 <b className="text-2xl font-normal">Change </b>
                 your password
               </h3>
               <form
                 onSubmit={handleSubmit(onSubmit)}
-                className=" mb-5 mx-auto max-w-[400px] text-center "
+                className=" mb-5 mx-auto max-w-[400px] text-center text-[#606060] "
               >
                 <div className="flex flex-col py-2">
                   <div className="flex flex-col justify-start items-start ">
                     <label className="font-medium">New Password</label>
                     <input
                       type="password"
-                      className="border-solid  w-full  border-2 border-[#cae0e7] px-3 py-2 outline-none focus:shadow-3xl focus:border-primary"
+                      className="border-solid  w-full  border-2  rounded-3xl px-3 py-2 outline-none"
                       {...register("password")}
                       autoComplete="off"
                     />
@@ -100,7 +103,7 @@ const RePassword = () => {
                     <label className="font-medium">Confirm Password</label>
                     <input
                       type="password"
-                      className="border-solid  w-full  border-2 border-[#cae0e7] px-3 py-2 outline-none focus:shadow-3xl focus:border-primary"
+                      className="border-solid  w-full  border-2  rounded-3xl px-3 py-2 outline-none"
                       {...register("confirmPassword")}
                       autoComplete="off"
                     />
@@ -113,7 +116,7 @@ const RePassword = () => {
                 </div>
                 <button
                   type="submit"
-                  className="bg-red-500 text-white block text-center py-2 px-5 w-full shadow-lg mt-4 mx-auto"
+                  className="bg-[#848abd] rounded-3xl text-white block text-center py-2 px-0 md:px-5 w-1/2 shadow-lg mt-4 mx-auto"
                 >
                   Change Password
                 </button>
@@ -121,8 +124,20 @@ const RePassword = () => {
             </div>
           ) : (
             <div className="w-full flex items-center justify-center relative">
-              <div className={`h-32 w-32 border-4 rounded-full ${submited? "border-green-600":"border-primary border-t-secondary"} animate-spin duration-1000 flex items-center justify-center `}></div>
-              <span className={`absolute ${submited? "text-green-600 animate-ping":""}`}>{submited?<Done className="text-6xl"/>:"Changing"}</span>
+              <div
+                className={`h-32 w-32 border-4 rounded-full ${
+                  submited
+                    ? "border-[#848abd]"
+                    : "border-primary border-t-[#848abd]"
+                } animate-spin duration-1000 flex items-center justify-center `}
+              ></div>
+              <span
+                className={`absolute ${
+                  submited ? "text-[#848abd] animate-ping" : ""
+                }`}
+              >
+                {submited ? <Done className="text-6xl" /> : "Changing"}
+              </span>
             </div>
           )}
         </div>
